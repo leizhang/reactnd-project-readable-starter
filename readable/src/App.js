3cfa8b4366675e08611d985cbe8f79bf2b5f61ae
jianleizhang <lei.zhang@cpit.ac.nz>
2017-09-12 23:39:32 +1200

    refactor: code tidy up

--- readable/src/App.js
+++ readable/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PostList from '../components/PostList';
+// import {
+//   Button,
+//   Container,
+//   Divider,
+//   Grid,
+//   Header,
+//   Image,
+//   Menu,
+//   Segment,
+// } from 'semantic-ui-react';
 
 class App extends Component {
+
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Container style={{ marginTop: '3em' }}>
+        <Header as="h1">Readable</Header>
+        <div>
+          <PostList />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Container>
     );
   }
 }
